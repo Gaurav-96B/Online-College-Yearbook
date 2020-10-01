4f0ec7c7472723e49a077c97f070ebe0fc4d48ec
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2020-10-01 23:31:27 +0530

    Update LogoutServlet.java

--- SourceCode/LogoutServlet.java
+++ SourceCode/LogoutServlet.java
@@ -1,1 +1,22 @@
+package FirstServlet;
+import java.io.*;
+import javax.servlet.*; 
+import javax.servlet.http.*; 
+import java.sql.*;
+import FirstServlet.DbConnection;
+public class LogoutServlet extends HttpServlet
+{  
+public void init()
+{	
+}
+public void doPost(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException  
+{
+HttpSession session=re.getSession(); 
+session.invalidate();
+response.sendRedirect("index.html");
+}
+public void destroy()
+{
+}
+}  
 
