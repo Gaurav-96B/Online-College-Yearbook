d3022e86555fbcd2476e446370fd9a9589fac152
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2020-10-01 23:15:30 +0530

    Update TeacherUpdate.java

--- SourceCode/TeacherUpdate.java
+++ SourceCode/TeacherUpdate.java
@@ -1,1 +1,36 @@
+package FirstServlet;
+import java.io.*;
+import javax.servlet.*; 
+import javax.servlet.http.*; 
+import java.sql.*;
+import FirstServlet.DbConnection;
+public class TeacherUpdate extends HttpServlet
+{  
+public void init()
+{
+}
+public void doPost(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException 
+{
+PrintWriter pw=res.getWriter();
+String username = req.getParameter("username");
+String email = req.getParameter("email");
+String id = req.getParameter("id");
+String address = req.getParameter("address");
+String mobile = req.getParameter("mobile");
+try
+{   
+PreparedStatement ps = con.prepareStatement("update reg set email='"+email+"',address='"+address+"',mobile='"+mobile+"' where username = '"+username+"' and id = '"+id+"'");
+ps.executeUpdate();
+res.sendRedirect("tupdateprofile.jsp?msg=success");
+}
+catch(Exception e1)
+{
+res.sendRedirect("tupdateprofile.jsp?m1=Failed");
+pw.println(e1.getMessage());
+}		
+}
+public void destroy()
+{
+}
+}  
 
