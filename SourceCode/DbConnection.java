9d94305f456e6876bd0768a9cb9d45ac8377bc4c
Gaurav-96B <72096518+Gaurav-96B@users.noreply.github.com>
2020-10-01 23:55:39 +0530

    Update DbConnection.java

--- SourceCode/DbConnection.java
+++ SourceCode/DbConnection.java
@@ -1,18 +1,19 @@
+package FirstServlet;
 import java.sql.*;
-public class Dbconnection 
+import java.util.*;
+
+public class DbConnection 
 {
-public static Connection getConnection() 
-{
-Connection con = null;
+Connection con=null;
 try 
 {
-Class.forName("com.mysql.jdbc.Driver");
-con = DriverManager.getConnection("jdbc:mysql://localhost:3306/yearbook", "root", "root");
-} 
-catch (Exception ex) 
-{
-ex.printStackTrace();
+Class.forName("com.mysql.jdbc.Driver");	
+con = DriverManager.getConnection("jdbc:mysql://localhost:3306/yearbook","root","root");
+String sql="";
 }
-return con;
+catch(Exception e)
+{
+System.out.println(e);
+}
 }
 }
